7d57c69e296e66b040196a46882c11f9e912063c
wlasah <agan.lawrence147@gmail.com>
2025-03-11 08:48:24 +0800

    Initial commit with project files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/To-Do-List-App/',  // ✅ Make sure to add the trailing slash
+  base: '/Todo-app-ga-test/',  // ✅ Make sure to add the trailing slash
 });
